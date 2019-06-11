a75e42a3c2e9da4d1df1b538008dd384f1e5ebef
Martin MacDonald <martin.macdonald01@bbc.co.uk>
2019-06-11 09:34:31 +0100

    added PORT

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const server = new ApolloServer({
 });
 
 server
-  .listen()
+  .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => {
     console.log(`Server ready at ${url}`);
   });
